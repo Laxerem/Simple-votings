6c6a4e737861cc19ba531217ad35b6fb33f4f26d
Grigory Vorobev <grishaMas@yandex.ru>
2025-02-16 14:07:56 +0500

    UPDATE: Корректировка этапа задач

--- Django/django_site/views.py
+++ Django/django_site/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from django_site.managers import UserManager
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
+from django.shortcuts import redirect
 
 User_model = get_user_model()
 
-# Create your views here.
+def main(request: HttpRequest) -> HttpResponse:
+    return redirect('home')
+
 def register(request: HttpRequest) -> HttpResponse:
     context = {}
     if request.method == "POST":
@@ -16,11 +19,21 @@ def register(request: HttpRequest) -> HttpResponse:
             context = {
                 'message': "Ваш аккаунт зарегистрирован"
             }
-
     return render(request, template_name="register.html", context=context)
 
+# Create your views here.
 def login(request: HttpRequest) -> HttpResponse:
-    return render(request, template_name="login.html")
+    context = {}
+    if request.method == "POST":
+        name = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, name, password)
+        if user:
+            context = {
+                'message': "Вы вошли в аккаунт"
+            }
+
+    return render(request, template_name="login.html", context=context)
 
 def home(request: HttpRequest) -> HttpResponse:
 
--- Django/src/urls.py
+++ Django/src/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from django_site.views import register, home, login
+from django_site.views import register, home, login, main
 
 urlpatterns = [
+    path("", main, name="main"),
     path('register', register, name="register"),
     path('login', login, name="login"),
     path('home', home, name="home")

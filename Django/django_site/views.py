fb4ddbef59ae82c1327aa57261ed5223127e027a
Grigory Vorobev <grishaMas@yandex.ru>
2025-02-17 14:43:50 +0500

    UPDATE: Теперь аккаунт можно удалить

--- Django/django_site/views.py
+++ Django/django_site/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from django_site.managers import UserManager
-from django.contrib.auth import get_user_model, authenticate, login
+from django.contrib.auth import get_user_model, authenticate, login, logout
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 
@@ -54,6 +54,21 @@ def user_login(request: HttpRequest) -> HttpResponse:
 
 def profile(request: HttpRequest) -> HttpResponse:
     return render(request, template_name="profile.html",)
+
+def user_logout(request: HttpRequest) -> HttpResponse:
+    logout(request)
+    return redirect("home")
+
+@login_required
+def delete_user(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        request.user.delete()
+        return redirect('home')
+    
+    context = {
+        "message": "Вы уверены что хотите удалить аккаунт?"
+    }
+    return render(request, template_name="profile_delete.html", context=context)
 
 def home(request: HttpRequest) -> HttpResponse:
     return render(request, template_name="home.html")
--- Django/src/urls.py
+++ Django/src/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from django_site.views import register, home, user_login, main, profile
+from django_site.views import register, home, user_login, main, profile, user_logout, delete_user
 
 urlpatterns = [
     path("", main, name="main"),
     path('register', register, name="register"),
     path('login', user_login, name="login"),
     path('home', home, name="home"),
-    path('profile', profile, name="profile")
+    path('profile', profile, name="profile"),
+    path('logout/', user_logout, name="logout"),
+    path('delete/user/', delete_user, name="profile_delete"),
 ]
